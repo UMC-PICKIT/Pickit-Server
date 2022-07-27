2d2069dee9db534f404ccdbb98b3d4cd7b384d17
KangSungBum <peter9706@naver.com>
2022-07-27 20:17:17 +0900

    feat:사용자 도메인 추가

--- src/main/java/com/example/pickit/domain/User.java
+++ src/main/java/com/example/pickit/domain/User.java
@@ -1,23 +1,28 @@
 package com.example.pickit.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter @Setter
 public class User {
 
     @Id
     @GeneratedValue
     private Long user_id;
+
     private String email;
     private String phone;
     private String nickName;
     private String userName;
+
+    @Embedded
+    private Address address;
+
     private String status;
 
     @CreatedDate
@@ -26,68 +31,5 @@ public class User {
 
     private LocalDateTime updatedAt;
 
-    public Long getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
 
