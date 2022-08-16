bd3be114e2e254c229a879ba3028e47544273f3c
KangSungBum <peter9706@naver.com>
2022-08-16 16:59:39 +0900

    refactor: base config 형식으로 서비스 수정

--- src/main/java/com/example/pickit/service/UserService.java
+++ src/main/java/com/example/pickit/service/UserService.java
@@ -1,13 +1,16 @@
 package com.example.pickit.service;
 
 import com.example.pickit.config.BaseException;
-import com.example.pickit.config.BaseResponseStatus;
 import com.example.pickit.domain.User;
+import com.example.pickit.dto.UserInfoDto;
+import com.example.pickit.repository.AddressRepository;
+import com.example.pickit.repository.OrderRepository;
 import com.example.pickit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.example.pickit.config.BaseResponseStatus.DATABASE_ERROR;
@@ -18,12 +21,18 @@ import static com.example.pickit.config.BaseResponseStatus.DATABASE_ERROR;
 public class UserService{
 
     private final UserRepository userRepository;
-
     @Transactional
-    public Long join(User user) {
-        validateDuplicateUser(user);
-        userRepository.saveUser(user);
-        return user.getId();
+    public Long join(User user) throws BaseException{
+        try {
+            validateDuplicateUser(user);
+            user.setStatus("ACTIVE");
+            user.setCreatedAt(LocalDateTime.now());
+            userRepository.saveUser(user);
+            return user.getId();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 
     private void validateDuplicateUser(User user) {
@@ -34,13 +43,23 @@ public class UserService{
     }
 
     @Transactional
-    public void updateUserInfo(Long userId, String updatedNickName) {
-        userRepository.updateUserInfo(userId, updatedNickName);
+    public void updateUserInfo(Long userId, String updatedNickName) throws BaseException{
+        try {
+            userRepository.updateUserInfo(userId, updatedNickName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 
     @Transactional
-    public void updateUserStatus(Long userId) {
-        userRepository.updateUserStatus(userId);
+    public void updateUserStatus(Long userId) throws BaseException {
+        try {
+            userRepository.updateUserStatus(userId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 
     public List<User> findUsers(){
@@ -52,6 +71,19 @@ public class UserService{
             return userRepository.findUser(id);
         } catch (Exception exception) {
             exception.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    @Transactional
+    public UserInfoDto findUser(Long userId) throws BaseException {
+        try {
+            User foundUser = userRepository.findUser(userId);
+            UserInfoDto returnDto = new UserInfoDto(foundUser.getUserName(), foundUser.getNickName(), foundUser.getEmail(), foundUser.getPhone(),foundUser.getStatus());
+            return returnDto;
+
+        } catch (Exception e) {
+            e.printStackTrace();
             throw new BaseException(DATABASE_ERROR);
         }
     }
