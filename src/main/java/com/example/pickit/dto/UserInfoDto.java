13c4c3fd003145aaa08dd73fdf9437e3deafab29
KangSungBum <peter9706@naver.com>
2022-08-16 16:57:15 +0900

    refactor: UserInfoDto 필드 수정

--- src/main/java/com/example/pickit/dto/UserInfoDto.java
+++ src/main/java/com/example/pickit/dto/UserInfoDto.java
@@ -2,20 +2,18 @@ package com.example.pickit.dto;
 
 import com.example.pickit.domain.Address;
 import com.example.pickit.domain.Order;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@AllArgsConstructor
 public class UserInfoDto {
-
     private String userName;
     private String nickName;
     private String email;
     private String phone;
-    private List<Address> addressList;
-    private List<Order> orderList;
     private String status;
-
 }
